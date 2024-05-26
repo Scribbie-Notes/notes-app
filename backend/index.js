d897b4fbf24285f83cb7e4625fc4e357cd817794
yashmandi <yashmandi18@gmail.com>
2024-05-26 16:11:15 +0530

    s

--- backend/index.js
+++ backend/index.js
@@ -75,6 +75,47 @@ app.post("/create-account", async (req, res) => {
 
 })
 
+// login
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email) {
+        return res.status(400).json({ error: true, message: "Please enter your email" });
+    }
+
+    if (!password) {
+        return res.status(400).json({ error: true, message: "Please enter your password" });
+    }
+
+    const userInfo = await User.findOne({ email: email })
+
+    if (!userInfo) {
+        return res.status(400).json({ message: "User not found" })
+    }
+
+    if (userInfo.email == email && userInfo.password == password) {
+        const user = { user: userInfo }
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "36000m"
+        });
+
+        return res.json({
+            error: false,
+            message: "Login successful",
+            email,
+            accessToken
+        });
+    } else {
+        return res.status(400).json({
+            error: true,
+            message: "Invalid credentials",
+        })
+    }
+
+})
+
+// add note
+
 app.listen(8000);
 
 module.exports = app;
