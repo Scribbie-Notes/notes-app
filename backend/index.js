b2673d526f959c7304db1da3d55c9bdc219e97c2
yashmandi <yashmandi18@gmail.com>
2024-06-03 17:45:06 +0530

    landing

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,9 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: "*"
+        origin: "*",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
     })
 );
 
