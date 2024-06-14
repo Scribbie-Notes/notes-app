603767dba0688eb82659f2a0300ba61f78785a38
yashmandi <yashmandi18@gmail.com>
2024-06-14 19:49:49 +0530

    axiosIns, index.js/backend

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./constants";
 
 const axiosInstance = axios.create({
-    baseURL: "https://scribbie-api.vercel.app",``
+    baseURL: "https://scribbie-api.vercel.app",
     timeout: 10000,
     headers: { "Content-Type": "application/json" },
     withCredentials: true,
