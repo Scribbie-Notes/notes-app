5b6618cf7fdf47f530071a7d6b8e0982eda1c87a
yashmandi <yashmandi18@gmail.com>
2024-06-28 11:58:30 +0530

    prod and dev .env files created

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-import { BASE_URL } from "./constants";
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     timeout: 10000,
     headers: { "Content-Type": "application/json" }
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    define: {
+      'import.meta.env': env,
+    },
+  }
 })
