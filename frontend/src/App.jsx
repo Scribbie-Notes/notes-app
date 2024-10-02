ef382eb219a46abe5f6269eb6d257cbb29c9629e
alaminpu1007 <alamin66.sit@gmail.com>
2024-10-02 20:44:59 +0600

    Tooltip features is integrated on dashboard component, added a loading skeletion to enhance user experience

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,68 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Signup from './pages/Signup/Signup';
-import Login from './pages/Login/Login';
-import Footer from './components/Footer';
-import Hero from './components/Hero/Hero';
-import About from './components/About/About';
-import Loading from './components/Loading';
-import ProfilePage from './pages/ProfilePage/ProfilePage';
-import Navbar from './components/Navbar';
-import ProtectedRoute from './utils/ProtectedRoute';
-import ErrorPage from './components/ErrorPage';
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
+import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
+import Loading from "./components/Loading";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+// currently this component is hide
+// import Navbar from './components/Navbar';
+// import ProtectedRoute from './utils/ProtectedRoute';
+// import ErrorPage from './components/ErrorPage';
+// import Footer from './components/Footer';
 
 const App = () => {
-  const [loading, setLoading] = useState(false);
-  const [user, setUser] = useState(null);
-  const location = useLocation();
+    const [loading, setLoading] = useState(false);
+    const [user, setUser] = useState(null);
+    const location = useLocation();
 
-  useEffect(() => {
-    const handleRouteChange = () => {
-      setLoading(true);
-      setTimeout(() => {
-        setLoading(false);
-      }, 500);
-    };
-    handleRouteChange();
-  }, [location]);
+    useEffect(() => {
+        const handleRouteChange = () => {
+            setLoading(true);
+            setTimeout(() => {
+                setLoading(false);
+            }, 500);
+        };
+        handleRouteChange();
+    }, [location]);
 
-  useEffect(() => {
-    const storedUser = localStorage.getItem('user');
-    if (storedUser) {
-      try {
-        setUser(JSON.parse(storedUser));
-      } catch (e) {
-        console.error('Error parsing stored user', e);
-      }
-    }
-  }, []);
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+            try {
+                setUser(JSON.parse(storedUser));
+            } catch (e) {
+                console.error("Error parsing stored user", e);
+            }
+        }
+    }, []);
 
-  return (
-    <div>
-      {loading && <Loading />}
-      {user && location.pathname === '/' ? (
-        <Navigate to='/dashboard' replace />
-      ) : (
-        <Routes>
-          <Route path='/landing' exact element={<Hero />} />
-          <Route path='/dashboard' exact element={<Home />} />
-          <Route path='/' exact element={<Hero />} />
-          <Route path='/login' exact element={<Login setUser={setUser} />} />
-          <Route path='/signup' exact element={<Signup />} />
-          <Route path='/about' exact element={<About />} />
-          <Route path='/my-profile' exact element={<ProfilePage />} />
-            {/* <Route path="/404" exact element={<ErrorPage/>} /> */}
-        </Routes>
-      )}
-    </div>
-  );
+    return (
+        <div>
+            {loading && <Loading />}
+            {user && location.pathname === "/" ? (
+                <Navigate to="/dashboard" replace />
+            ) : (
+                <Routes>
+                    <Route path="/landing" exact element={<Hero />} />
+                    <Route path="/dashboard" exact element={<Home />} />
+                    <Route path="/" exact element={<Hero />} />
+                    <Route path="/login" exact element={<Login setUser={setUser} />} />
+                    <Route path="/signup" exact element={<Signup />} />
+                    <Route path="/about" exact element={<About />} />
+                    <Route path="/my-profile" exact element={<ProfilePage />} />
+                    {/* <Route path="/404" exact element={<ErrorPage/>} /> */}
+                </Routes>
+            )}
+        </div>
+    );
 };
 
 const AppWithRouter = () => (
-  <Router>
-    <App />
-  </Router>
+    <Router>
+        <App />
+    </Router>
 );
 
 export default AppWithRouter;
