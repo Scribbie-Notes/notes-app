b9629387ee28e935426d5d194d1d3bb7c9eb9feb
saurabh-dev-vns <saurabhsahani0001@gmail.com>
2024-10-03 07:49:15 +0530

    noteModel updated

--- backend/models/noteModel.js
+++ backend/models/noteModel.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const noteSchema = new Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
-    tags: { type: [String], default: [] }, // Ensure this is an array
+    tags: { type: [String], default: [] },
+	attachments:{type:[String],default:[]},
     isPinned: { type: Boolean, required: false },
     userId: { type: String, required: true },
     createdOn: { type: Date, default: Date.now }
