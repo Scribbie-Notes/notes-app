72a3a4f4cd420356815f3243e494604eb88043f5
Nayan Mahera <maheranayan18@gmail.com>
2024-10-03 13:39:39 +0530

    feat:#45 modern footer added

---
+++ frontend/src/components/sticky_footer/Content.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+
+const navLinks = [
+  { link: "/about", name: "About Us" },
+  { link: "/contact", name: "Contact Us" },
+  { link: "/feedback", name: "Feedback" },
+  { link: "/support", name: "Help & Support" },
+  {
+    link: "https://github.com/yashmandi/notes-app",
+    name: "GitHub",
+    external: true,
+  },
+  { link: "/app-version", name: "App Version" },
+];
+
+export default function Content() {
+  return (
+    <div className="bg-[#111827] pt-24 py-8 px-12 h-full w-full flex flex-col justify-between">
+      <nav className="grid max-lg:grid-cols-2 mx-auto grid-cols-6 gap-4 text-gray-400">
+        {navLinks.map(({ link, name, external }, index) => (
+          <NavLink key={index} link={link} name={name} external={external} />
+        ))}
+      </nav>
+      <Nav />
+      <Footer />
+    </div>
+  );
+}
+
+const NavLink = ({ link, name, external }) => (
+  <a
+    className="hover:text-white duration-300 flex justify-center items-center w-48"
+    href={link}
+    target={external ? "_blank" : "_self"}
+    rel={external ? "noreferrer" : undefined}
+  >
+    {name}
+  </a>
+);
+
+const Footer = () => {
+  const [isWide, setIsWide] = useState(window.innerWidth > 640);
+
+  useEffect(() => {
+    const handleResize = () => setIsWide(window.innerWidth > 640);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <section className="text-white">
+      {!isWide && (
+        <div className="flex justify-center mb-4">
+          <img
+            className="w-20 bg-white p-2 rounded-3xl h-20"
+            alt="logo"
+            src="/logo.png"
+          />
+        </div>
+      )}
+      <div
+        className={`flex ${
+          isWide ? "justify-between items-end" : "flex-col items-center"
+        } text-white`}
+      >
+        <h1
+          className={`${
+            isWide ? "text-[14vw]" : "text-[12vw] mt-10"
+          } leading-[0.8]`}
+        >
+          Scribbie {!isWide && <br />}
+        </h1>
+        <p className={isWide ? "" : "mt-8"}>©2024 by Scribbie</p>
+      </div>
+    </section>
+  );
+};
+
+const Nav = () => {
+  return (
+    <div className="flex justify-center sm:gap-20 mt-8">
+      <div className="w-full sm:w-2/3">
+        <h1 className="text-white mb-8 text-center text-xl sm:text-3xl">
+          Unlocking creativity and productivity through intuitive note-taking
+          and seamless organization.
+        </h1>
+      </div>
+    </div>
+  );
+};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { Toaster } from 'react-hot-toast';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId={import.meta.env.VITE_REACT_APP_GOOGLE_API_TOKEN}>
+createRoot(document.getElementById("root")).render(
+  <GoogleOAuthProvider
+    clientId={import.meta.env.VITE_REACT_APP_GOOGLE_API_TOKEN}
+  >
     <React.StrictMode>
       <App />
       <Toaster />
