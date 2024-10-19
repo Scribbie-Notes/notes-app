683b419326b40fc81004b2e1542d86a36b8a6f10
AADESHak007 <123197632+AADESHak007@users.noreply.github.com>
2024-10-19 08:32:30 +0530

    fixed the ui of the button in navbar

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -80,7 +80,7 @@ const Home = () => {
     } catch (error) {
       if (error.response.status === 401) {
         localStorage.clear();
-        navigate("/login");
+        navigate("/login")
       }
     }
   };
