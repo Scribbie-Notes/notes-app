158589446ea9ee6fa4df32649f68b2a9bc92e9d4
Mehul <mehulprajapati2802@gmail.com>
2024-11-04 12:43:59 -0500

    Added view modal for archive notes

--- frontend/src/components/ArchivedNotes/ArchivedNotes.jsx
+++ frontend/src/components/ArchivedNotes/ArchivedNotes.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import Navbar from '../Navbar';
 import axiosInstance from '../../utils/axiosInstance';
 import NoteCard from '../Cards/NoteCard';
-import { MdColorLens, MdDelete, MdOutlinePushPin, MdOutlineUnarchive, MdPushPin } from 'react-icons/md';
+import { MdColorLens, MdDelete, MdOutlinePushPin, MdOutlineUnarchive, MdPushPin, MdClose} from 'react-icons/md';
 import toast from 'react-hot-toast';
 import axios from 'axios';
 import AddEditNotes from "../../pages/Home/AddEditNotes";
+import moment from "moment";
+import ReactQuill from 'react-quill';
 
 const ArchivedNotes = () => {
   const user = JSON.parse(localStorage.getItem("user"));
@@ -20,6 +22,10 @@ const ArchivedNotes = () => {
   const [openAddEditModal, setOpenAddEditModal] = useState({
     isShown: false,
     type: "add",
+    data: null,
+  });
+  const [viewNoteModal, setViewNoteModal] = useState({
+    isShown: false,
     data: null,
   });
 
@@ -213,6 +219,10 @@ const ArchivedNotes = () => {
     setOpenAddEditModal({ isShown: true, type: "edit", data: noteDetails });
   };
 
+  const handleViewNote = (noteDetails) => {
+    setViewNoteModal({ isShown: true, data: noteDetails });
+  };
+
   return (
     <div>
       {selectedNotes.length > 0 ? (
@@ -346,6 +356,37 @@ const ArchivedNotes = () => {
                             getAllNotes={getArchivedNotes}
                         />
                     </div>
+                </div>
+            )}
+            {viewNoteModal.isShown && (
+                <div className="fixed inset-0 flex items-center justify-center z-50">
+                <div className="absolute inset-0 bg-black opacity-50"></div>
+                <div className="relative bg-white p-5 rounded-lg shadow-lg z-10 w-[90%] sm:w-[80%] md:w-[60%] lg:w-[40%] max-h-3/4 overflow-hidden">
+                    <button
+                    className="absolute top-3 right-3 text-gray-900 transition-all bg-gray-50 hover:bg-red-100 hover:text-gray-500 focus:outline-none font-medium rounded-full text-sm px-2.5 py-2.5 text-xs"
+                    onClick={() => setViewNoteModal({ isShown: false, data: null })}
+                    >
+                    <MdClose className="text-xl text-slate-400" />
+                    </button>
+                    <div className="overflow-auto">
+                    <h2 className="text-2xl font-semibold">
+                        {viewNoteModal.data.title}
+                    </h2>
+                    <span className="text-xs text-slate-500">
+                        {moment(viewNoteModal.data.date).format("Do MMM YYYY")}
+                    </span>
+                    <p className="text-gray-700 mt-4"><ReactQuill value={viewNoteModal.data.content} readOnly={true} theme="bubble" /></p>
+                    <div className="mt-4">
+                        {viewNoteModal.data.tags.map((tag, index) => (
+                        <span
+                            key={index}
+                            className="inline-block bg-gray-100 mr-2 text-gray-800 text-xs font-medium px-1.5 py-0.5 rounded dark:bg-blue-100 dark:text-gray-800"
+                        >#{tag}
+                        </span>
+                        ))}
+                    </div>
+                    </div>
+                </div>
                 </div>
             )}
           </>
